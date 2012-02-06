3f725bf3e046b3794c288d4568693eefe76211f0
Ryan Horn <rhorn@sococo.net>
2012-02-05 19:44:14 -0800

    Event emitting for status and delete notices

--- test/TwitterStreamApi.js
+++ test/TwitterStreamApi.js
@@ -1,5 +1,7 @@
 var assert = require('assert'),
-    TwitterStreamApi = require('../lib/TwitterStreamApi.js');
+    TwitterStreamApi = require('../lib/TwitterStreamApi.js'),
+    http = require('http'),
+    EventEmitter = require('events').EventEmitter;
 
 describe('TwitterStreamApi.Client', function() {
     it('Should require a username', function() {
@@ -21,3 +23,56 @@ describe('TwitterStreamApi.Client', function() {
     });
 });
 
+describe('TwitterStreamApi.ResponseProcessor', function() {
+    it('Should emit statuses', function(done) {
+        var mockResp = new EventEmitter();
+        var emitter = new EventEmitter();
+
+        var mockStatuses = [
+            {text: 'Status 1'},
+            {text: 'Status 2'},
+            {text: 'Status 3'}
+        ];
+
+        var receivedStatuses = [];
+
+        var inc = 0;
+        emitter.on('status', function(status) {
+            assert.equal(status.text, mockStatuses[inc++].text);
+            if (3 == inc) {
+                done();
+            }
+        });
+
+        var respProcessor = new TwitterStreamApi.ResponseProcessor(mockResp, emitter);
+
+        mockStatuses.forEach(function(status) {
+            mockResp.emit('data', JSON.stringify(status) + '\r');
+        });
+    });
+
+    it('Should emit delete notices', function(done) {
+        var mockResp = new EventEmitter();
+        var emitter = new EventEmitter();
+
+        var mockStatuses = [
+            {text: 'Status 1'},
+            {'delete': {status: {text: 'deleted status'}}},
+            {text: 'Status 3'}
+        ];
+
+        var receivedStatuses = [];
+
+        var inc = 0;
+        emitter.on('delete', function(status) {
+            assert.equal(status.text, 'deleted status');
+            done();
+        });
+
+        var respProcessor = new TwitterStreamApi.ResponseProcessor(mockResp, emitter);
+
+        mockStatuses.forEach(function(status) {
+            mockResp.emit('data', JSON.stringify(status) + '\r');
+        });
+    });
+});
