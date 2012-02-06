cde044249f1f979708c7d801186c9cf437b6d9d2
Ryan Horn <rhorn@sococo.net>
2012-02-05 18:18:04 -0800

    TwitterStreamApi.client is now TwitterStreamApi.Client. TwitterStreamApi.responseProcessor is now TwitterStreamApi.ResponseProcessor

--- test/TwitterStreamApi.js
+++ test/TwitterStreamApi.js
@@ -1,21 +1,21 @@
 var assert = require('assert'),
     TwitterStreamApi = require('../lib/TwitterStreamApi.js');
 
-describe('TwitterStreamApi.client', function() {
+describe('TwitterStreamApi.Client', function() {
     it('Should require a username', function() {
         assert.throws(function() {
-            var api = new TwitterStreamApi.client();
+            var api = new TwitterStreamApi.Client();
         }, /username is required/);
     });
 
     it('Should require a password', function() {
         assert.throws(function() {
-            var api = new TwitterStreamApi.client({username: 'test'});
+            var api = new TwitterStreamApi.Client({username: 'test'});
         }, /password is required/);
     });
 
     it('Should set auth http option and default host', function() {
-        var api = new TwitterStreamApi.client({username: 'test', password: 'pass'});
+        var api = new TwitterStreamApi.Client({username: 'test', password: 'pass'});
         assert.equal(api.httpOptions.host, 'stream.twitter.com');
         assert.equal(api.httpOptions.auth, 'test:pass');
     });
