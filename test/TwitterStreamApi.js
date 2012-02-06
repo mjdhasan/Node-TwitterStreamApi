c1658ad749edb0aec6dfaf1d16463ea8815dd903
Ryan Horn <rhorn@sococo.net>
2012-02-05 16:24:25 -0800

    Password is now required

--- test/TwitterStreamApi.js
+++ test/TwitterStreamApi.js
@@ -7,4 +7,10 @@ describe('TwitterStreamApi.client', function() {
             var api = new TwitterStreamApi.client();
         }, /username is required/);
     });
+
+    it('Should require a password', function() {
+        assert.throws(function() {
+            var api = new TwitterStreamApi.client({username: 'test'});
+        }, /password is required/);
+    });
 });
